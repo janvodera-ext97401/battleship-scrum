301fdacae67dc8725f00129e66147fe0f666b3c3
Jan Voděra <jan.vodera@ext.csas.cz>
2023-06-06 11:23:22 +0200

    empty gameplay service

---
+++ src/main/kotlin/gameplay/GameplayServiceImpl.kt
@@ -0,0 +1,17 @@
+package kotlin.gameplay
+
+import model.Player
+
+class GameplayServiceImpl : GameplayService {
+    override fun startGame() {
+        TODO("Not yet implemented")
+    }
+
+    override fun endGame() {
+        TODO("Not yet implemented")
+    }
+
+    override fun whoIsOnTurn(): Player {
+        TODO("Not yet implemented")
+    }
+}
