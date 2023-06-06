409c5050b3b841a737ce416adeb761feacfea15c
Jan Voděra <jan.vodera@ext.csas.cz>
2023-06-06 12:27:16 +0200

    remove redundant code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,9 +2,6 @@
 import battleplan.BattlePlanServiceImpl
 import gameplay.GameplayServiceImpl
 import model.Player
-import model.Point
-import java.util.*
-import kotlin.collections.ArrayList
 
 
 fun main(args: Array<String>) {
@@ -30,17 +27,6 @@ fun main(args: Array<String>) {
 
 
     }
-}
-
-fun createPointFrom(stringInput: String): Point {
-    val alphabet = "abcdefghijklmnopqrstuvwxyz";
-
-    val firstChar = stringInput.substring(0, 1).lowercase(Locale.getDefault());
-    var firstIndex = alphabet.indexOf(firstChar);
-    val secondChar = stringInput.substring(1, 2);
-    val secondIndex = Integer.parseInt(secondChar) - 1;
-
-    return Point(firstIndex, secondIndex)
 }
 
 
