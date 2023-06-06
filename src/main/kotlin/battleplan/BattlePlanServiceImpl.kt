854a2d7588ca4ec60cf3ccf8378c604c4c383f66
SSS <cen55519@mpydl9nqnqhq.cen.csin.cz>
2023-06-06 11:23:36 +0200

    create class BattlePlanServiceImpl

---
+++ src/main/kotlin/battleplan/BattlePlanServiceImpl.kt
@@ -0,0 +1,18 @@
+package battleplan
+
+import model.Point
+import model.Ship
+
+class BattlePlanServiceImpl : BattlePlanService {
+    override fun createBoard(size: Int): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun addShip(ship: Ship) {
+        TODO("Not yet implemented")
+    }
+
+    override fun shot(playerName: String, point: Point) {
+        TODO("Not yet implemented")
+    }
+}
